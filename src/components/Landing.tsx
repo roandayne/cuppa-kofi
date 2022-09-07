ddd03b60a6cbbaa123b77c19fea0eae3bd0246b2
Roan Dayne Dino <rdayne@acidinc.ca>
2022-09-07 20:30:02 +0800

    add landing page

---
+++ src/components/Landing.tsx
@@ -0,0 +1,26 @@
+import { Box, Text, Image } from '@mantine/core'
+import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
+import React from 'react'
+import Navigation from './Navigation'
+import useStyles from "./styles/Landing.styles"
+
+const Landing = () => {
+  const { classes } = useStyles()
+
+  return (
+    <Box className={classes.container}>
+      <Navigation />
+
+      <Image 
+        height={"100vh"}
+        style={{ position: "absolute", height: "100%" }} 
+        src='./landing-bg.jpg'
+        alt="coffee-shop-bg"
+      />
+
+      <Text className={classes.header}>cuppa kofi.</Text>
+    </Box>
+  )
+}
+
+export default Landing
---
+++ src/components/styles/Landing.styles.ts
@@ -0,0 +1,27 @@
+import { createStyles } from "@mantine/core";
+
+export default createStyles(() => ({
+  container: {
+    height: "100vh",
+    width: "100vw",
+    position: "relative",
+  },
+  header: {
+    position: "absolute",
+    top: "45%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    fontSize: "8rem",
+    // color: "black",
+    // textShadow: "1px 1px rgba(255,255,255,0.8)"
+
+    // color: "transparent",
+    // background: "black",
+    // backgroundClip: "text",
+    // textShadow: "0px 3px 3px rgba(255,255,255,0.8)",
+
+    color: "rgba(0,0,0,1)",
+    background: "transparent",
+    textShadow: "3px 2px 3px rgba(255,255,255,.2)",
+  }
+}))
