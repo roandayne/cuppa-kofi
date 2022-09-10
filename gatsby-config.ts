b1c13bc494506b1bfaf1d832a828d37f2df594bf
Roan Dayne Dino <rdayne@acidinc.ca>
2022-09-11 00:55:16 +0800

    add title and favico

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,11 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    "gatsby-plugin-react-helmet", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { BoxProps, Box } from '@mantine/core'
 import React from 'react'
 import Navigation from './Navigation'
 import Footer from './Footer'
+import { Helmet } from 'react-helmet';
 
 interface LayoutProps extends BoxProps {
   children?: React.ReactNode;
@@ -9,14 +10,26 @@ interface LayoutProps extends BoxProps {
 
 const Layout: React.FC<LayoutProps> = ({children, ...props}) => {
   return (
-    <Box sx={{
-      display: "flex",
-      flexDirection: "column",
-    }}>
-      <Navigation />
-      {children}
-      <Footer />
-    </Box>
+    <>
+      <Helmet>
+        <title>Cuppa Kofi</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/logo.png"
+        />
+      </Helmet>
+      <Box sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        <Navigation />
+        {children}
+        <Footer />
+      </Box>
+    </>
+    
   )
 }
 
